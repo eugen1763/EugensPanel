e9d532afa14a96263b06b147a5678247f8f44ea8
Finn Hubo <fhubo@timeline.de>
2023-07-10 11:52:10 +0200

    added floawbite with basic theming

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { DarkMode } from "flowbite-svelte";
+  import "../app.postcss";
+</script>
+
+<div class="absolute w-screen h-screen bg-white dark:bg-slate-800 bg-container">
+  <slot />
+
+  <DarkMode />
+</div>
+
+<style>
+  .bg-container {
+    background-image: radial-gradient(
+      circle,
+      #aaa3 1px,
+      rgba(0, 0, 0, 0) 1px
+    );
+    background-size: 1rem 1rem;
+  }
+</style>
